751ce8abc1f5b5c5a4fb9e30509c2d6b0df9ffbc
QimingZheng <qimingzheng96@outlook.com>
2020-06-19 17:32:34 +0800

    update

--- testings/metric.py
+++ testings/metric.py
@@ -15,12 +15,17 @@ def metric(label, result):
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
-        data = json.load(f)
-    q = data["query"]
-    ground_truth = data["heap"]
-    del data["query"]
-    del data["heap"]
-    for filter_type, results in data.items():
-        for method, result in results.items():
-            precision, recall = metric(ground_truth[method], result)
-            print(filter_type, method, precision, recall)
+        lines = f.readlines()
+    for line in lines:
+        data = json.loads(line)
+        q = data["query"]
+        ground_truth = data["heap"]
+        del data["query"]
+        del data["heap"]
+        for filter_type, results in data.items():
+            for method, result in results.items():
+                try:
+                    precision, recall = metric(ground_truth[method], result)
+                    print(filter_type, method, precision, recall)
+                except:
+                    print("erro")
